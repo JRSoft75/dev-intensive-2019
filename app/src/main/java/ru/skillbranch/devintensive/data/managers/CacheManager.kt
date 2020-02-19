1e303a777adb3bd6087aa4594e1c1483671168d2
Ryabchenko E <johny@mail.ru>
2020-02-19 14:40:08 +0300

    +

--- app/src/main/java/ru/skillbranch/devintensive/data/managers/CacheManager.kt
+++ app/src/main/java/ru/skillbranch/devintensive/data/managers/CacheManager.kt
@@ -1,9 +1,28 @@
 package ru.skillbranch.devintensive.data.managers
 
+import ru.skillbranch.devintensive.extensions.mutableLiveData
+import ru.skillbranch.devintensive.models.data.Chat
+import ru.skillbranch.devintensive.models.data.User
+
 object CacheManager {
-    val chats = mutableLiveData(DataGenerator.stabChats)
+    private val chats = mutableLiveData(DataGenerator.stabChats)
+    private val users = mutableLiveData(DataGenerator.stabUsers)
 
     fun loadChats(){
         return chats
+    }
+
+    fun findUserByIds(ids: List<String>) : List<User> {
+        return users.value!!.filter{ids.contains(it.id)}
+    }
+
+    fun nextChatId(): String{
+        return "${chats.value!!.size}"
+    }
+
+    fun insertChat(chat: Chat){
+        val copy = chats.value!!.toMutableList()
+        copy.add(chat)
+        chats.value = copy
     }
 }
